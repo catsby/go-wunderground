f553473e02bcda00a16fad3d396825765eb14ffd
Logan Kennelly <logank@sent.com>
2017-11-29 00:30:05 -0800

    wutest: Add more interesting data, planner

--- wutest/sample.go
+++ wutest/sample.go
@@ -1804,7 +1804,7 @@ func GetTestApiResponse() *wu.ApiResponse{
                 "min": "56",
                 "tzname": "UTC"
                 },
-                "tempm":"15.6", "tempi":"60.1","dewptm":"12.2", "dewpti":"54.0","hum":"80","wspdm":"9.3", "wspdi":"5.8","wgustm":"-9999.0", "wgusti":"-9999.0","wdird":"170","wdire":"South","vism":"16.1", "visi":"10.0","pressurem":"1015.5", "pressurei":"29.99","windchillm":"-999", "windchilli":"-999","heatindexm":"-9999", "heatindexi":"-9999","precipm":"-9999.00", "precipi":"-9999.00","conds":"Mostly Cloudy","icon":"mostlycloudy","fog":"0","rain":"0","snow":"0","hail":"0","thunder":"0","tornado":"0","metar":"METAR KSFO 250856Z 17005KT 10SM BKN150 16/12 A2999 RMK AO2 SLP155 T01560122 58015 $" },
+                "tempm":"15.6", "tempi":"60.1","dewptm":"12.2", "dewpti":"54.0","hum":"80","wspdm":"9.3", "wspdi":"5.8","wgustm":"-9999.0", "wgusti":"-9999.0","wdird":"170","wdire":"South","vism":"16.1", "visi":"10.0","pressurem":"1015.5", "pressurei":"29.99","windchillm":"-999", "windchilli":"-999","heatindexm":"-9999", "heatindexi":"-9999","precipm":"-9999.00", "precipi":"-9999.00","conds":"Mostly Cloudy","icon":"mostlycloudy","fog":"1","rain":"0","snow":"1","hail":"0","thunder":"0","tornado":"0","metar":"METAR KSFO 250856Z 17005KT 10SM BKN150 16/12 A2999 RMK AO2 SLP155 T01560122 58015 $" },
                 {
                 "date": {
                 "pretty": "1:56 AM PST on November 25, 2017",
@@ -1824,7 +1824,7 @@ func GetTestApiResponse() *wu.ApiResponse{
                 "min": "56",
                 "tzname": "UTC"
                 },
-                "tempm":"15.6", "tempi":"60.1","dewptm":"12.2", "dewpti":"54.0","hum":"80","wspdm":"0.0", "wspdi":"0.0","wgustm":"-9999.0", "wgusti":"-9999.0","wdird":"0","wdire":"North","vism":"16.1", "visi":"10.0","pressurem":"1015.3", "pressurei":"29.99","windchillm":"-999", "windchilli":"-999","heatindexm":"-9999", "heatindexi":"-9999","precipm":"-9999.00", "precipi":"-9999.00","conds":"Scattered Clouds","icon":"partlycloudy","fog":"0","rain":"0","snow":"0","hail":"0","thunder":"0","tornado":"0","metar":"METAR KSFO 250956Z 00000KT 10SM SCT150 16/12 A2998 RMK AO2 SLP153 T01560122 $" },
+                "tempm":"15.6", "tempi":"60.1","dewptm":"12.2", "dewpti":"54.0","hum":"80","wspdm":"0.0", "wspdi":"0.0","wgustm":"-9999.0", "wgusti":"-9999.0","wdird":"0","wdire":"North","vism":"16.1", "visi":"10.0","pressurem":"1015.3", "pressurei":"29.99","windchillm":"-999", "windchilli":"-999","heatindexm":"-9999", "heatindexi":"-9999","precipm":"-9999.00", "precipi":"-9999.00","conds":"Scattered Clouds","icon":"partlycloudy","fog":"0","rain":"0","snow":"0","hail":"1","thunder":"0","tornado":"0","metar":"METAR KSFO 250956Z 00000KT 10SM SCT150 16/12 A2998 RMK AO2 SLP153 T01560122 $" },
                 {
                 "date": {
                 "pretty": "2:56 AM PST on November 25, 2017",
@@ -2276,7 +2276,7 @@ func GetTestApiResponse() *wu.ApiResponse{
                 "min": "00",
                 "tzname": "America/Los_Angeles"
                 },
-                "fog":"0","rain":"0","snow":"0","snowfallm":"0.00", "snowfalli":"0.00","monthtodatesnowfallm":"0.00", "monthtodatesnowfalli":"0.00","since1julsnowfallm":"0.00", "since1julsnowfalli":"0.00","snowdepthm":"", "snowdepthi":"","hail":"0","thunder":"0","tornado":"0","meantempm":"17", "meantempi":"62","meandewptm":"12", "meandewpti":"54","meanpressurem":"1014", "meanpressurei":"29.96","meanwindspdm":"7", "meanwindspdi":"4","meanwdire":"","meanwdird":"93","meanvism":"16", "meanvisi":"10","humidity":"","maxtempm":"20", "maxtempi":"68","mintempm":"13", "mintempi":"56","maxhumidity":"86","minhumidity":"55","maxdewptm":"14", "maxdewpti":"57","mindewptm":"9", "mindewpti":"49","maxpressurem":"1015", "maxpressurei":"29.99","minpressurem":"1014", "minpressurei":"29.94","maxwspdm":"34", "maxwspdi":"21","minwspdm":"0", "minwspdi":"0","maxvism":"16", "maxvisi":"10","minvism":"16", "minvisi":"10","gdegreedays":"12","heatingdegreedays":"3","coolingdegreedays":"0","precipm":"0.00", "precipi":"0.00","precipsource":"","heatingdegreedaysnormal":"11","monthtodateheatingdegreedays":"158","monthtodateheatingdegreedaysnormal":"217","since1sepheatingdegreedays":"234","since1sepheatingdegreedaysnormal":"391","since1julheatingdegreedays":"294","since1julheatingdegreedaysnormal":"504","coolingdegreedaysnormal":"0","monthtodatecoolingdegreedays":"0","monthtodatecoolingdegreedaysnormal":"0","since1sepcoolingdegreedays":"214","since1sepcoolingdegreedaysnormal":"66","since1jancoolingdegreedays":"346","since1jancoolingdegreedaysnormal":"163" }
+                "fog":"1","rain":"0","snow":"1","snowfallm":"0.00", "snowfalli":"0.00","monthtodatesnowfallm":"25.40", "monthtodatesnowfalli":"1.00","since1julsnowfallm":"0.00", "since1julsnowfalli":"0.00","snowdepthm":"", "snowdepthi":"","hail":"0","thunder":"0","tornado":"0","meantempm":"17", "meantempi":"62","meandewptm":"12", "meandewpti":"54","meanpressurem":"1014", "meanpressurei":"29.96","meanwindspdm":"7", "meanwindspdi":"4","meanwdire":"","meanwdird":"93","meanvism":"16", "meanvisi":"10","humidity":"","maxtempm":"20", "maxtempi":"68","mintempm":"13", "mintempi":"56","maxhumidity":"86","minhumidity":"55","maxdewptm":"14", "maxdewpti":"57","mindewptm":"9", "mindewpti":"49","maxpressurem":"1015", "maxpressurei":"29.99","minpressurem":"1014", "minpressurei":"29.94","maxwspdm":"34", "maxwspdi":"21","minwspdm":"0", "minwspdi":"0","maxvism":"16", "maxvisi":"10","minvism":"16", "minvisi":"10","gdegreedays":"12","heatingdegreedays":"3","coolingdegreedays":"0","precipm":"0.00", "precipi":"0.00","precipsource":"","heatingdegreedaysnormal":"11","monthtodateheatingdegreedays":"158","monthtodateheatingdegreedaysnormal":"217","since1sepheatingdegreedays":"234","since1sepheatingdegreedaysnormal":"391","since1julheatingdegreedays":"294","since1julheatingdegreedaysnormal":"504","coolingdegreedaysnormal":"0","monthtodatecoolingdegreedays":"0","monthtodatecoolingdegreedaysnormal":"0","since1sepcoolingdegreedays":"214","since1sepcoolingdegreedaysnormal":"66","since1jancoolingdegreedays":"346","since1jancoolingdegreedaysnormal":"163" }
                 ]
         }
                 ,
@@ -11575,3 +11575,238 @@ func GetTestApiResponse() *wu.ApiResponse{
 	return ar
 }
 
+// Returns a new ApiResponse with a planner set.
+func GetTestPlanner() *wu.ApiResponse{
+	text := `
+{
+  "response": {
+  "version":"0.1",
+  "termsofService":"http://www.wunderground.com/weather/api/d/terms.html",
+  "features": {
+  "planner": 1
+  }
+        }
+                ,
+        "trip": {
+                "title": "Historical Summary for July 01 - July 31",
+                "airport_code": "KSFO",
+                "error": "",
+                "period_of_record": {
+                "date_start": {
+                "date":{
+        "epoch":"1341169200",
+        "pretty":"12:00 PM PDT on July 01, 2012",
+        "day":1,
+        "month":7,
+        "year":2012,
+        "yday":182,
+        "hour":12,
+        "min":"00",
+        "sec":0,
+        "isdst":"1",
+        "monthname":"July",
+        "monthname_short":"Jul",
+        "weekday_short":"Sun",
+        "weekday":"Sunday",
+        "ampm":"PM",
+        "tz_short":"PDT",
+        "tz_long":"America/Los_Angeles"
+}
+                },
+                "date_end": {
+                "date":{
+        "epoch":"1501527600",
+        "pretty":"12:00 PM PDT on July 31, 2017",
+        "day":31,
+        "month":7,
+        "year":2017,
+        "yday":211,
+        "hour":12,
+        "min":"00",
+        "sec":0,
+        "isdst":"1",
+        "monthname":"July",
+        "monthname_short":"Jul",
+        "weekday_short":"Mon",
+        "weekday":"Monday",
+        "ampm":"PM",
+        "tz_short":"PDT",
+        "tz_long":"America/Los_Angeles"
+}
+                }
+                },
+                "temp_high": {
+                "min": {
+                "F": "63",
+                "C": "17"
+                },
+                "avg": {
+                "F": "72",
+                "C": "22"
+                },
+                "max": {
+                "F": "90",
+                "C": "32"
+                }
+                },
+                "temp_low": {
+                "min": {
+                "F": "51",
+                "C": "11"
+                },
+                "avg": {
+                "F": "56",
+                "C": "14"
+                },
+                "max": {
+                "F": "65",
+                "C": "18"
+                }
+                },
+                "precip": {
+                "min": {
+                "in": "0.00",
+                "cm": "0.0"
+                },
+                "avg": {
+                "in": "0.00",
+                "cm": "0.0"
+                },
+                "max": {
+                "in": "0.00",
+                "cm": "0.0"
+                }
+                },
+                "dewpoint_high": {
+                "min": {
+                "F": "49",
+                "C": "9"
+                },
+                "avg": {
+                "F": "55",
+                "C": "13"
+                },
+                "max": {
+                "F": "66",
+                "C": "19"
+                }
+                },
+                "dewpoint_low": {
+                "min": {
+                "F": "43",
+                "C": "6"
+                },
+                "avg": {
+                "F": "51",
+                "C": "11"
+                },
+                "max": {
+                "F": "57",
+                "C": "14"
+                }
+                },
+                "cloud_cover": {
+                "cond": "mostly sunny"
+                },
+                "chance_of": {
+                "tempoversixty": {
+                "name": "Warm",
+                "description": "temperature over 60&deg;F / 16&deg;C and below 90&deg;F / 32&deg;C",
+                "percentage": "99"
+                },
+                "chanceofwindyday": {
+                "name": "Windy",
+                "description": "average wind over 10 mph / 15km/h",
+                "percentage": "75"
+                },
+                "chanceofpartlycloudyday": {
+                "name": "Partly Cloudy",
+                "description": "",
+                "percentage": "51"
+                },
+                "chanceofsunnycloudyday": {
+                "name": "Sunny",
+                "description": "",
+                "percentage": "40"
+                },
+                "chanceofcloudyday": {
+                "name": "Cloudy",
+                "description": "",
+                "percentage": "10"
+                },
+                "chanceofhumidday": {
+                "name": "Humid",
+                "description": "dew point over 60&deg;F / 16&deg;C",
+                "percentage": "6"
+                },
+                "chanceoffogday": {
+                "name": "Fog",
+                "description": "",
+                "percentage": "5"
+                },
+                "chanceofprecip": {
+                "name": "Precipitation",
+                "description": "",
+                "percentage": "3"
+                },
+                "chanceofrainday": {
+                "name": "Rain",
+                "description": "",
+                "percentage": "3"
+                },
+                "chanceofthunderday": {
+                "name": "Thunderstorms",
+                "description": "",
+                "percentage": "1"
+                },
+                "tempoverninety": {
+                "name": "Hot",
+                "description": "temperature over 90&deg;F / 32&deg;C",
+                "percentage": "1"
+                },
+                "chanceofsnowonground": {
+                "name": "Ground Snow",
+                "description": "",
+                "percentage": "0"
+                },
+                "chanceoftornadoday": {
+                "name": "Tornado",
+                "description": "",
+                "percentage": "0"
+                },
+                "chanceofsultryday": {
+                "name": "Sweltering",
+                "description": "dew point over 70&deg;F / 21&deg;C",
+                "percentage": "0"
+                },
+                "tempbelowfreezing": {
+                "name": "Freezing",
+                "description": "temperature below 32&deg;F / 0&deg;C",
+                "percentage": "0"
+                },
+                "tempoverfreezing": {
+                "name": "Cool",
+                "description": "temperature between 32&deg;F / 0&deg;C and 60&deg;F / 16&deg; C",
+                "percentage": "0"
+                },
+                "chanceofhailday": {
+                "name": "Hail",
+                "description": "",
+                "percentage": "0"
+                },
+                "chanceofsnowday": {
+                "name": "Snow",
+                "description": "",
+                "percentage": "0"
+                }
+                }
+        }
+}
+`
+
+	ar := &wu.ApiResponse{}
+	if err := json.Unmarshal([]byte(text), &ar); err != nil {
+		panic(err)
+	}
+	return ar
+}
